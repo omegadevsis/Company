527f5327501948c7cb0bc4daeb074ba2a609bff1
Omegasis <61483070+omegadevsis@users.noreply.github.com>
2026-02-09 14:01:57 -0300

    Adicionado dbcontext e serviceextensions

---
+++ Company.Presentation/Extensions/ServiceExtension.cs
@@ -0,0 +1,19 @@
+﻿using Company.Infrastructure.Persistence;
+using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Presentation.Extensions;
+
+public static class ServiceExtension
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        string? connectionString)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseMySQL(connectionString!);
+        });
+        
+        return services;
+    }
+}
